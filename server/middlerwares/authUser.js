7b6c37cdcfc7e28d0cb3cbf0f57a751f22b7c49b
Rimjhim <148185321+rsaxena4147@users.noreply.github.com>
2025-05-13 02:29:15 +0530

    Update authUser.js

--- server/middlerwares/authUser.js
+++ server/middlerwares/authUser.js
@@ -4,7 +4,7 @@ import jwt from 'jsonwebtoken'
 const authUser = async (req, res, next) => {
   const { token } = req.cookies;
   if (!token) {
-    return res.json({ success: false });
+    return res.json({ success: false  , message:"Please Login"});
   }
 
   try {
