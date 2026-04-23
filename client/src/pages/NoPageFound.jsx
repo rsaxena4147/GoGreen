50334e9f824655a60cc02e4419746172d993be65
Rimjhim <148185321+rsaxena4147@users.noreply.github.com>
2026-04-23 21:52:06 +0530

    added file

---
+++ client/src/pages/NoPageFound.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function NoPageFound() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-9xl font-bold text-gray-800">
+        404
+      </h1>
+    </div>
+  )
+}
+
+export default NoPageFound
